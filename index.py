b2bb75099bf340882ee622308e6463d36047702c
zakhmatovk <zahmatovk@mail.ru>
2024-09-21 21:40:13 +0300

    Support response from gpt

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import json
 from logging import getLogger
 from typing import TypedDict
+
+from pydantic import BaseModel
 from migrate import apply_migrations
 from alice_types.request import AliceRequest
 from alice_types.response import AliceResponse
@@ -9,18 +11,30 @@ from clients.ya_gpt import client
 log = getLogger()
 
 CLASSIFY_PROMT = '''
-Нужно классифицировать запрос `class` и выделить сущность `entity` из запроса
+Нужно классифицировать запрос `operation` и выделить сущность `entity` из запроса
 
 классы запросов: ["out_of_stock", "add_stock" ,"task"]
 
 примеры:
-"Кончилась паста" - {"class": "out_of_stock" , "entity": "паста"}
-"Добавь таблетки для посудомойки в список покупок" - {"class": "out_of_stock" , "entity": "таблетки для посудомоечной машины"}
-"Я купил капсулы для стиралки" - {"class": "add_stock" , "entity": "капсулы для стиральной машины"}
+"Кончилась паста" - {"operation": "out_of_stock" , "entity": "паста"}
+"Добавь таблетки для посудомойки в список покупок" - {"operation": "out_of_stock" , "entity": "таблетки для посудомоечной машины"}
+"Я купил капсулы для стиралки" - {"operation": "add_stock" , "entity": "капсулы для стиральной машины"}
 
 Дай ответ в формате json
 
 '''
+
+
+class RecognizedOperation(BaseModel):
+    operation: str
+    entity: str
+
+    def message(self):
+        if self.operation == 'out_of_stock':
+            return f'Я так понимаю, что {self.entity} больше нет'
+        if self.operation == 'add_stock':
+            return f'Запас {self.entity} пополнен'
+        return 'Это что-то новенькое'
 
 
 class Event(TypedDict):
@@ -31,7 +45,16 @@ class Event(TypedDict):
 
 async def process(request: AliceRequest, reply: AliceResponse):
     r = await client.req(CLASSIFY_PROMT, request.request.original_utterance)
-    log.warn(json.dumps(r))
+    text = None
+    for it in r['result']['alternatives']:
+        if it['status'] == 'ALTERNATIVE_STATUS_FINAL':
+            text = it['message']['text']
+    if text is None:
+        return None
+
+    operation = RecognizedOperation.model_validate_json(text.strip('`'))
+    reply.response.text = operation.message()
+    reply.response.end_session = True
 
 
 async def handler(event: Event, context):
@@ -48,9 +71,10 @@ async def handler(event: Event, context):
     alice_request = AliceRequest.model_validate(event)
     reply = AliceResponse()
 
-    classify_result = await process(alice_request, reply)
-
-    reply.response.text = alice_request.request.original_utterance
-    reply.response.end_session = True
+    try:
+        classify_result = await process(alice_request, reply)
+    except Exception as e:
+        reply.response.text = 'Во время обработки запроса что-то пошло не так'
+        reply.response.end_session = True
 
     return reply.model_dump()
