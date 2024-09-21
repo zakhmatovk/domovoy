fbc1847953501d6375514aff05ad332574336c4b
zakhmatovk <zahmatovk@mail.ru>
2024-09-21 20:54:33 +0300

    Add gpt client

---
+++ clients/ya_gpt.py
@@ -0,0 +1,62 @@
+import os
+from typing import Literal, TypedDict
+
+import aiohttp
+
+YA_GPT_API_TOKEN = os.environ['YA_GPT_API_TOKEN']
+FOLDER_ID = 'b1gac1g0nm3qptu01u57'
+
+
+class CompletionOptions(TypedDict):
+    stream: bool
+    temperature: float
+    maxTokens: int
+
+
+class MessageGPT(TypedDict):
+    role: Literal['system', 'user', 'assistant']
+    text: str
+
+
+class RequestGTP(TypedDict):
+    modelUri: str
+    completionOptions: CompletionOptions
+    messages: list[MessageGPT]
+
+
+class ResponseAlternativeGPT(TypedDict):
+    message: MessageGPT
+    status: str
+
+
+class ResponseResultGPT(TypedDict):
+    alternatives: list[ResponseAlternativeGPT]
+
+
+class ResponseGPT(TypedDict):
+    result: ResponseResultGPT
+
+
+class YaGPTClient:
+    base_url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
+
+    def __init__(self) -> None:
+        pass
+
+    async def req(self, promt_text: str, message_text: str) -> ResponseGPT:
+        promt: RequestGTP = {
+            'modelUri': f'gpt://{FOLDER_ID}/yandexgpt/latest',
+            'completionOptions': {'stream': False, 'temperature': 0.0, 'maxTokens': 2000},
+            'messages': [{'role': 'system', 'text': promt_text}, {'role': 'user', 'text': message_text}],
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url=self.base_url,
+                json=promt,
+                headers={"Content-Type": "application/json", "Authorization": f"Api-Key {YA_GPT_API_TOKEN}"},
+            ) as response:
+                response.raise_for_status()
+                return await response.json()
+
+
+client = YaGPTClient()
--- index.py
+++ index.py
@@ -1,28 +1,20 @@
-import asyncio
 import json
-import os
-from ydb_pool import ydb_pool
+from logging import getLogger
+from typing import TypedDict
 from migrate import apply_migrations
 
-# Параметры подключения
-YDB_ENDPOINT = os.getenv('YDB_ENDPOINT')
-YDB_DATABASE = os.getenv('YDB_DATABASE')
-YDB_HAS_CREDENTIALS = os.getenv('YDB_DATABASE', '').capitalize() == 'True'
-POOL_SIZE = 5
+log = getLogger()
 
 
-async def select_one():
-    query = 'SELECT 1;'
-    try:
-        print('request', query)
-        r = await ydb_pool.execute(query=query)
-        return r
-    except Exception as e:
-        print(f'An error occurred: {e}')
+class Event(TypedDict):
+    body: str | None
+    version: str
+    session: str
 
 
-async def handler(event, context):
+async def handler(event: Event, context):
     body = json.loads(event.get('body') or '{}')
+
     if body.get('method') == 'apply_migrations':
         try:
             await apply_migrations()
@@ -30,13 +22,9 @@ async def handler(event, context):
         except Exception as e:
             return {'statusCode': 500, 'body': {'exception': str(e)}}
     # Execute query with the retry_operation helper.
-    result = await select_one()
+    log.warning(json.dumps(body))
     return {
-        'statusCode': 200,
-        'body': result[0].rows,
+        "version": event["version"],
+        "session": event["session"],
+        "response": {"text": 'Пытаюсь понять ответ', "end_session": True},
     }
-
-
-# Выполнение функции в асинхронном контексте
-if __name__ == '__main__':
-    asyncio.run(select_one())
