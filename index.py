418aba67a77cfc2f61650d3a37d9c398d1d8ba59
zakhmatovk <zahmatovk@mail.ru>
2024-09-21 21:23:12 +0300

    test gpt reply

--- index.py
+++ index.py
@@ -4,14 +4,34 @@ from typing import TypedDict
 from migrate import apply_migrations
 from alice_types.request import AliceRequest
 from alice_types.response import AliceResponse
+from clients.ya_gpt import client
 
 log = getLogger()
+
+CLASSIFY_PROMT = '''
+Нужно классифицировать запрос `class` и выделить сущность `entity` из запроса
+
+классы запросов: ["out_of_stock", "add_stock" ,"task"]
+
+примеры:
+"Кончилась паста" - {"class": "out_of_stock" , "entity": "паста"}
+"Добавь таблетки для посудомойки в список покупок" - {"class": "out_of_stock" , "entity": "таблетки для посудомоечной машины"}
+"Я купил капсулы для стиралки" - {"class": "add_stock" , "entity": "капсулы для стиральной машины"}
+
+Дай ответ в формате json
+
+'''
 
 
 class Event(TypedDict):
     body: str | None
     version: str
     session: str
+
+
+async def process(request: AliceRequest, reply: AliceResponse):
+    r = await client.req(CLASSIFY_PROMT, request.request.original_utterance)
+    log.warn(json.dumps(r))
 
 
 async def handler(event: Event, context):
@@ -26,14 +46,11 @@ async def handler(event: Event, context):
             return {'statusCode': 500, 'body': {'exception': str(e)}}
 
     alice_request = AliceRequest.model_validate(event)
-
     reply = AliceResponse()
 
-    # if alice_request.is_new_session():
-    #     reply.response.text = "Привет, скажи что-нибудь и я это повторю"
-    # else:
-    reply.response.text = alice_request.request.original_utterance
+    classify_result = await process(alice_request, reply)
 
-    reply_body = reply.model_dump()
-    log.warning(json.dumps(body))
-    return reply_body
+    reply.response.text = alice_request.request.original_utterance
+    reply.response.end_session = True
+
+    return reply.model_dump()
